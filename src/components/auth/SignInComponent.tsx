6e5d27b419bf1775f3382dc2f0932f60d3bb9027
Balamathias <balamathias05@gmail.com>
2024-07-03 00:16:25 +0100

    Recent update

--- src/components/auth/SignInComponent.tsx
+++ src/components/auth/SignInComponent.tsx
@@ -55,7 +55,7 @@ const SignInComponent = () => {
             console.error(error)
             setIsPending(false)
             setStatus(true)
-            setError(error.message === 'fetch failed' ? 'Make sure you are connected to the internet to continue.' : error?.message)
+            setError(error.message === 'fetch failed' ? 'Make sure you are connected to the internet to continue.' : 'Make sure the details yyou entered are correct. You may want to double-check your Password or Email.')
         }
         finally { setIsPending(false) }
       }
