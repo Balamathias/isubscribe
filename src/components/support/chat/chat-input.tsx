b1b9683356296468b98313667a3380547d628a23
Balamathias <balamathias05@gmail.com>
2024-08-24 03:05:04 +0100

    You can now press Enter to send

--- src/components/support/chat/chat-input.tsx
+++ src/components/support/chat/chat-input.tsx
@@ -2,12 +2,13 @@
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { useCreateChat, useGetUserChatRooms } from '@/lib/react-query/funcs/chats'
+import { useCreateChat, useCreateChatRoom, useGetUserChatRooms } from '@/lib/react-query/funcs/chats'
 import { QueryKeys } from '@/lib/react-query/query-keys'
+import { updateChatRoomParticipants } from '@/lib/supabase/chats'
 import { Tables } from '@/types/database'
 import { useMutationState, useQueryClient } from '@tanstack/react-query'
 import { LucideSend } from 'lucide-react'
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 import { toast } from 'sonner'
 
@@ -19,16 +20,30 @@ const ChatInput = () => {
   const sP = useSearchParams()
   const chatRoomId = sP.get('chat_room_id')
 
-  const variables = useMutationState<Tables<'chats'>>({
-    filters: { mutationKey: [QueryKeys.create_chat, chatRoomId], status: 'pending' },
-    select: (mutation) => mutation.state.variables as any,
-  })
+  const { mutate: createChatRoom, isPending: creatingChatRoom } = useCreateChatRoom()
+  const router = useRouter() 
+
+  const handleCreateRoom = () => {
+    createChatRoom(undefined, {
+        onSuccess: async (data) => {
+          queryClient.invalidateQueries({queryKey: [QueryKeys.get_user_chats]})
+          queryClient.invalidateQueries({queryKey: [QueryKeys.get_user_chats, data?.id]})
+          await updateChatRoomParticipants(chatRoomId!)
+          createChat({message, room_id: data?.id!})
+          router.replace('?chat_room_id=' + data?.id )
+        }
+    })
+}
 
   const handleSubmit = () => {
     if (!message) return toast.info('Please type a message before sending.')
-      if (!chatRoomId) return toast.info('Please Start a New Chat.')
+      if (!chatRoomId) {
+        handleCreateRoom()
+        setMessage('')
+        return
+      }
 
-      createChat({message, room_id: chatRoomId}, {
+      createChat({message, room_id: chatRoomId!}, {
         onSuccess: () => {
           setMessage('')
           queryClient.invalidateQueries({queryKey: [QueryKeys.get_user_chats, chatRoomId]})
@@ -49,6 +64,11 @@ const ChatInput = () => {
                 className="flex-1 border rounded-lg p-2 h-12 bg-background dark:bg-secondary dark:border-none"
                 placeholder="Type your message..."
                 onChange={(e) => setMessage(e.target.value)}
+                onKeyPress={(e) => {
+                  if (e.key === 'Enter') {
+                    handleSubmit();
+                  }
+                }}
                 value={message}
             />
             <Button onClick={handleSubmit} variant={'secondary'} disabled={creating} className="bg-primary/20 text-primary rounded-lg flex items-center justify-center text-center hover:opacity-70 h-12 p-4">
