9884a875fd3cb20b088956d1429245e288a8bd3b
Balamathias <balamathias05@gmail.com>
2024-08-25 11:28:11 +0100

    Testing privacy policy

--- src/app/(info)/layout.tsx
+++ src/app/(info)/layout.tsx
@@ -1,9 +1,16 @@
+import Logo from '@/components/Logo'
 import React, { PropsWithChildren } from 'react'
 
 const InfoLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className='flex flex-col w-full h-full'>
         {children}
+
+        <footer className='flex items-center py-5 px-4 justify-between w-full bg-secondary'>
+          <Logo />
+
+          <p className='text-muted-foreground whitespace-nowrap'>&copy; iSubscribe {new Date().getFullYear()}.</p>
+        </footer>
     </div>
   )
 }
--- src/app/(info)/privacy-policy/page.tsx
+++ src/app/(info)/privacy-policy/page.tsx
@@ -1,8 +1,18 @@
+import { Card } from '@/components/ui/card'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div className='flex flex-col max-w-7xl'>Page</div>
+    <div className='flex flex-col p-4 gap-y-5 mx-auto w-full py-8 min-h-screen max-w-7xl'>
+      <h2 className="text-2xl md:text-3xl font-semibold">iSubscribe Privacy Policy</h2>
+
+      <Card className='py-6 px-4 md:px-5 flex flex-col border-none gap-y-4 rounded-lg shadow-none !bg-purple-500/20 text-purple-500'>
+        <h2 className='text-base font-semibold md:text-xl'>How we use your data?</h2>
+        <p className='text-sm md:text-base'>We may use your `email` and `username` to auto-create your iSubscribe virtual account when you sign up for the first time, or to verify your identity as the case may be.</p>
+        <p className='text-sm md:text-base'>iSubscribe cannot read your PIN or Password as the Passwords and Pins are hashed using the bcrypt package. This is to ensure that a layer of security is added.</p>
+        <p className='text-sm md:text-base'>We might use your email to update your with news of our recent updates.</p>
+      </Card>
+    </div>
   )
 }
 
