00be11c7ce709f11023de281f60a9a59a6155825
Ladalo-Daniel <balaladalo@gmail.com>
2024-10-18 03:17:08 +0100

    removed moribund ring-1 className from about page

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -9,25 +9,27 @@ const Header = async () => {
   const {data: {user}} = await getCurrentUser()
 
   return (
-    <div className=' flex flex-row justify-between items-center bg-inherit backdrop-blur-md border-b h-[70px] top-0 sticky shadow-l px-2 z-50'>
-      <div>
-      <Logo showLogoText />
-      </div>
-      <div className='flex items-center justify-between gap-x-3'>
-        <Button asChild variant={'secondary'} className='rounded-full w-full dark:bg-secondary bg-white ring-1 font-semibold border-none' size={'lg'}>
-            <Link 
-              href={'/about'} 
-              className={``}
-            >About us</Link>
-        </Button>
-         {!user && <div className='md:flex hidden flex-row items-center gap-x-2'>
-            <Button asChild variant={'secondary'} className='rounded-full w-full dark:bg-secondary bg-white ring-1 font-semibold border-none' size={'lg'}>
-              <Link href={"/sign-up"}>Sign up</Link>
-            </Button>
-            <Button asChild className='rounded-full w-full border-none' size={'lg'}>
-              <Link href={"/sign-in"}>Login</Link>
-            </Button>
-          </div>}
+    <div className=' flex flex-row justify-center items-center w-full bg-inherit backdrop-blur-md border-b h-[70px] top-0 sticky shadow-l px-2 z-50'>
+      <div className='flex max-w-7xl items-center justify-between gap-x-4 w-full'>
+        <div>
+          <Logo showLogoText />
+        </div>
+        <div className='flex items-center justify-between gap-x-3'>
+          <Button asChild variant={'secondary'} className='rounded-full w-full dark:bg-secondary bg-white ring-1 font-semibold border-none' size={'lg'}>
+              <Link 
+                href={'/about'} 
+                className={``}
+              >About us</Link>
+          </Button>
+          {!user && <div className='md:flex hidden flex-row items-center gap-x-2'>
+              <Button asChild variant={'secondary'} className='rounded-full w-full dark:bg-secondary bg-white ring-1 font-semibold border-none' size={'lg'}>
+                <Link href={"/sign-up"}>Sign up</Link>
+              </Button>
+              <Button asChild className='rounded-full w-full border-none' size={'lg'}>
+                <Link href={"/sign-in"}>Login</Link>
+              </Button>
+            </div>}
+        </div>
       </div>
     </div>
   )
