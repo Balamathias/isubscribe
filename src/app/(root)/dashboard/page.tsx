19c4ff87ac5e731dd6d4358a93dbbd2fd5f60ccc
Ladalo-Daniel <balaladalo@gmail.com>
2024-10-19 19:23:09 +0100

    Damped rate us

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -7,7 +7,6 @@ import RecentTransactions from '@/components/dashboard/recent-transactions'
 import ActionBoxSkeleton from '@/components/skeletons/ActionBoxSkeleton'
 import { getUser } from '@/lib/supabase/accounts'
 import { getWallet } from '@/lib/supabase/wallets'
-import { getGreeting } from '@/lib/utils'
 import React, { Suspense } from 'react'
 
 const DashboardPage = async ({}: { searchParams: {[key: string]: string} }) => {
@@ -20,7 +19,7 @@ const DashboardPage = async ({}: { searchParams: {[key: string]: string} }) => {
     <WidthWrapper className='flex flex-col space-y-2.5 sm:space-y-4 !max-w-3xl md:py-12 mt-16 max-sm:mt-8'>
 
       <div className='flex flex-col space-y-1 md:hidden'>
-        <h2 className='text-muted-foreground text-lg'>{getGreeting(profile?.full_name?.split(' ')?.at(0) ?? '')}</h2>
+        <h2 className='text-muted-foreground text-lg'>Hi, <span className="font-semibold dark:text-amber-500/90">{profile?.full_name?.split(' ')?.at(0)}</span>.</h2>
         <p className='text-muted-foreground text-xs'>What would you love to subscribe today?</p>
       </div>
 
