e3e9407e1e400463627e8a0f0e7ca02254a7bc4a
Balamathias <balamathias05@gmail.com>
2024-12-16 02:48:36 +0100

    Speed optimizations

--- src/app/(root)/dashboard/layout.tsx
+++ src/app/(root)/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import Bottombar from '@/components/dashboard/navigation/Bottombar'
 import Sidebar from '@/components/dashboard/navigation/Sidebar'
 import Topbar from '@/components/dashboard/navigation/Topbar'
+import { getUser } from '@/lib/supabase/accounts'
 import { Metadata } from 'next'
 import React, { PropsWithChildren } from 'react'
 
@@ -9,13 +10,15 @@ export const metadata:Metadata = {
     description: 'Your home of affordable utility bills.',
 }
 
-const Layout = ({ children }: PropsWithChildren) => {
+const Layout = async ({ children }: PropsWithChildren) => {
+
+  const { data: user } = await getUser()
 
   return (
     <div className='bg-violet-50/90 dark:bg-gray-900 flex min-h-screen w-full overflow-hidden relative'>
        <Sidebar />
           <div className="flex flex-col w-full relative overflow-auto custom-scrollbar ml-2 md:ml-[180px]">
-              <Topbar />
+              <Topbar profile={user} />
               { children }
           </div>
         <Bottombar />
--- src/components/dashboard/navigation/Topbar.tsx
+++ src/components/dashboard/navigation/Topbar.tsx
@@ -18,17 +18,13 @@ import { motion } from 'framer-motion'
 import { ArrowRight, LucideUserCheck, LucideInfo } from 'lucide-react'
 import InstallButton from '@/components/install-pwa-button'
 import { Button } from '@/components/ui/button'
-import { useGetProfile } from '@/lib/react-query/funcs/user'
-import { Skeleton } from '@/components/ui/skeleton'
 
 
-const Topbar = () => {
+const Topbar = ({ profile }: { profile?: Tables<'profile'> | null }) => {
   const pathname = usePathname()
   const [openSupport, setOpenSupport] = useState(false)
   const [openProfileActions, setOpenProfileActions] = useState(false)
   const [isInstallable, setIsInstallable] = useState(false)
-  const { data, isPending } = useGetProfile()
-  const profile = data?.data
 
   useEffect(() => { 
     setOpenSupport(false)
@@ -69,98 +65,89 @@ const Topbar = () => {
           }
 
           <>
-            {
-              isPending ? (
-                <div className='flex flex-row gap-x-8 items-center'>
-                  <Skeleton className="h-7 w-20 rounded-md md:block hidden" />
-                  <Skeleton className="h-10 w-10 rounded-full" />
-                </div>
-              ): (
-                <DynamicModal
-                  open={openProfileActions}
-                  setOpen={setOpenProfileActions}
-                  trigger = {
-                    <Link passHref href={'#'} 
-                      className='cursor-pointer hover:opacity-80 flex items-center gap-x-1'>
-                      <span className='text-muted-foreground text-xs hidden md:block'>{getGreeting()}, <span className="font-semibold dark:text-amber-500/90">{profile?.full_name?.split(' ')?.at(0) || 'Guest'}</span>!</span>
-                      <Avatar title={profile?.full_name ?? 'Guest'}>
-                        <AvatarImage src={profile?.avatar!}/>
-                        <AvatarFallback>{profile?.full_name?.[0] || 'G'}</AvatarFallback>
-                      </Avatar>
-                    </Link>
-                  }
-                >
-                  <div className='flex flex-col gap-y-2 justify-between md:p-3 md:py-1 py-1 p-1.5'>
-                    <Link passHref href={'#'} 
-                      className='cursor-pointer hover:opacity-80 flex items-center gap-x-1 pb-4 py-2'>
-                      <Avatar title={profile?.full_name ?? 'Guest'}>
-                        <AvatarImage src={profile?.avatar!}/>
-                        <AvatarFallback>{profile?.full_name?.[0] || 'G'}</AvatarFallback>
-                      </Avatar>
-                      <div className='flex flex-col space-y-1'>
-                        <p className='text-muted-foreground text-xs'>{profile?.full_name || 'Guest'}</p>
-                        <p className='text-muted-foreground text-sm'>{profile?.email || 'Please sign in to continue'}</p>
-                      </div>
-                    </Link>
-
-                    <div className="flex flex-col space-y-3 mt-8">
-
-                      <div className='flex items-center gap-x-2 text-muted-foreground cursor-pointer hover:text-foreground hover:transition-all peer peer-hover:opacity-65'>
-                        <span className='w-10 h-10 flex items-center justify-center rounded-full bg-secondary/20 text-foreground'>
-                          <LucideSettings2 size={17} strokeWidth={1.5} className='' />
-                        </span>
-                        <Link href={'/dashboard/settings'}>Preferences</Link>
-                      </div>
-
-                      <div className='flex items-center gap-x-2 text-muted-foreground cursor-pointer hover:text-foreground hover:transition-all peer peer-hover:opacity-65'>
-                        <span className='w-10 h-10 flex items-center justify-center rounded-full bg-secondary/20 text-foreground'>
-                          <LucideUserCheck size={17} strokeWidth={1.5} className='' />
-                        </span>
-                        <Link href={'/privacy-policy'}>Privacy Policy</Link>
-                      </div>
-
-                      <div className='flex items-center gap-x-2 text-muted-foreground cursor-pointer hover:text-foreground hover:transition-all peer peer-hover:opacity-65'>
-                        <span className='w-10 h-10 flex items-center justify-center rounded-full bg-secondary/20 text-foreground'>
-                          <LucideInfo size={17} strokeWidth={1.5} className='' />
-                        </span>
-                        <Link href={'/about'}>About us</Link>
-                      </div>
-
-                      <div className='flex items-center gap-x-2 text-muted-foreground cursor-pointer hover:text-foreground hover:transition-all peer peer-hover:opacity-65'>
-                        <span className='w-10 h-10 flex items-center justify-center rounded-full bg-secondary/20 text-foreground'>
-                          <LucideShare2 size={17} strokeWidth={1.5} className='' />
-                        </span>
-                        <ComingSoon 
-                          trigger={<Link href={'#'}>Share and earn</Link>}
-                          message='We are working very hard to bring this feature to you. Very soon, you will be able to share and earn.'
-                        />
-                      </div>
-
-                    </div>
-                    
-                    <div className='mt-auto'>
-                      <SignOutComponent profile={profile} setOpenProfileActions={setOpenProfileActions} />
-                    </div>
-
-                    {
-                      !profile && (
-                        <Link href="/sign-in" className='w-full'>
-                          <motion.button
-                            className="flex items-center justify-center gap-2 py-2 px-3 bg-violet-600 text-white shadow-lg hover:bg-violet-700 dark:bg-violet-600 dark:hover:bg-violet-700 transition-all ease-in-out duration-300 rounded-full w-full"
-                            initial={{ opacity: 0, scale: 0.9 }}
-                            animate={{ opacity: 1, scale: 1 }}
-                            transition={{ duration: 0.4, ease: 'easeInOut', delay: 0.35 }}
-                          >
-                            Sign In
-                            <ArrowRight className="h-5 w-5" />
-                          </motion.button>
-                        </Link>
-                      )
-                    }
+            <DynamicModal
+              open={openProfileActions}
+              setOpen={setOpenProfileActions}
+              trigger = {
+                <Link passHref href={'#'} 
+                  className='cursor-pointer hover:opacity-80 flex items-center gap-x-1'>
+                  <span className='text-muted-foreground text-xs hidden md:block'>{getGreeting()}, <span className="font-semibold dark:text-amber-500/90">{profile?.full_name?.split(' ')?.at(0) || 'Guest'}</span>!</span>
+                  <Avatar title={profile?.full_name ?? 'Guest'}>
+                    <AvatarImage src={profile?.avatar!}/>
+                    <AvatarFallback>{profile?.full_name?.[0] || 'G'}</AvatarFallback>
+                  </Avatar>
+                </Link>
+              }
+            >
+              <div className='flex flex-col gap-y-2 justify-between md:p-3 md:py-1 py-1 p-1.5'>
+                <Link passHref href={'#'} 
+                  className='cursor-pointer hover:opacity-80 flex items-center gap-x-1 pb-4 py-2'>
+                  <Avatar title={profile?.full_name ?? 'Guest'}>
+                    <AvatarImage src={profile?.avatar!}/>
+                    <AvatarFallback>{profile?.full_name?.[0] || 'G'}</AvatarFallback>
+                  </Avatar>
+                  <div className='flex flex-col space-y-1'>
+                    <p className='text-muted-foreground text-xs'>{profile?.full_name || 'Guest'}</p>
+                    <p className='text-muted-foreground text-sm'>{profile?.email || 'Please sign in to continue'}</p>
                   </div>
-                </DynamicModal>
-              )
-            }
+                </Link>
+
+                <div className="flex flex-col space-y-3 mt-8">
+
+                  <div className='flex items-center gap-x-2 text-muted-foreground cursor-pointer hover:text-foreground hover:transition-all peer peer-hover:opacity-65'>
+                    <span className='w-10 h-10 flex items-center justify-center rounded-full bg-secondary/20 text-foreground'>
+                      <LucideSettings2 size={17} strokeWidth={1.5} className='' />
+                    </span>
+                    <Link href={'/dashboard/settings'}>Preferences</Link>
+                  </div>
+
+                  <div className='flex items-center gap-x-2 text-muted-foreground cursor-pointer hover:text-foreground hover:transition-all peer peer-hover:opacity-65'>
+                    <span className='w-10 h-10 flex items-center justify-center rounded-full bg-secondary/20 text-foreground'>
+                      <LucideUserCheck size={17} strokeWidth={1.5} className='' />
+                    </span>
+                    <Link href={'/privacy-policy'}>Privacy Policy</Link>
+                  </div>
+
+                  <div className='flex items-center gap-x-2 text-muted-foreground cursor-pointer hover:text-foreground hover:transition-all peer peer-hover:opacity-65'>
+                    <span className='w-10 h-10 flex items-center justify-center rounded-full bg-secondary/20 text-foreground'>
+                      <LucideInfo size={17} strokeWidth={1.5} className='' />
+                    </span>
+                    <Link href={'/about'}>About us</Link>
+                  </div>
+
+                  <div className='flex items-center gap-x-2 text-muted-foreground cursor-pointer hover:text-foreground hover:transition-all peer peer-hover:opacity-65'>
+                    <span className='w-10 h-10 flex items-center justify-center rounded-full bg-secondary/20 text-foreground'>
+                      <LucideShare2 size={17} strokeWidth={1.5} className='' />
+                    </span>
+                    <ComingSoon 
+                      trigger={<Link href={'#'}>Share and earn</Link>}
+                      message='We are working very hard to bring this feature to you. Very soon, you will be able to share and earn.'
+                    />
+                  </div>
+
+                </div>
+                
+                <div className='mt-auto'>
+                  <SignOutComponent profile={profile} setOpenProfileActions={setOpenProfileActions} />
+                </div>
+
+                {
+                  !profile && (
+                    <Link href="/sign-in" className='w-full'>
+                      <motion.button
+                        className="flex items-center justify-center gap-2 py-2 px-3 bg-violet-600 text-white shadow-lg hover:bg-violet-700 dark:bg-violet-600 dark:hover:bg-violet-700 transition-all ease-in-out duration-300 rounded-full w-full"
+                        initial={{ opacity: 0, scale: 0.9 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{ duration: 0.4, ease: 'easeInOut', delay: 0.35 }}
+                      >
+                        Sign In
+                        <ArrowRight className="h-5 w-5" />
+                      </motion.button>
+                    </Link>
+                  )
+                }
+              </div>
+            </DynamicModal>
           </>
         </div>
       </div>
