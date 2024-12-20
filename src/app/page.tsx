199ae685bb2a350bcf16447d854de3da3eba4a3d
Ladalo-Daniel <balaladalo@gmail.com>
2024-12-20 23:55:38 +0100

    Revert to splashscreen home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,32 @@
-// 'use client'
+'use client'
 
-// import { useRouter } from "next/navigation"
-// import { useEffect } from "react"
-// import SplashScreen from "@/components/splash-screen"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
+import SplashScreen from "@/components/splash-screen"
 
-import HomePage from '@/components/home/HomePage'
-import { createVirtualAccount } from '@/lib/paystack/services'
+// import HomePage from '@/components/home/HomePage'
 
-export default async function Home({searchParams}: { searchParams: {[key: string]: string }}) {
-  // const router = useRouter()
+export default function Home({searchParams}: { searchParams: {[key: string]: string }}) {
+  const router = useRouter()
   
-  // useEffect(() => {
-  //   const params = new URLSearchParams(searchParams)
-  //   const errorCode = params.get('error_code')
-  //   const errorMessage = params.get('error_description')
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams)
+    const errorCode = params.get('error_code')
+    const errorMessage = params.get('error_description')
 
-  //   if (errorCode === '403') {
-  //     router.push('/auth/auth-code-error?msg=' + encodeURIComponent(errorMessage!))
-  //     return
-  //   }
+    if (errorCode === '403') {
+      router.push('/auth/auth-code-error?msg=' + encodeURIComponent(errorMessage!))
+      return
+    }
 
-  //   const timeoutId = setTimeout(() => {
-  //     router.push('/dashboard')
-  //   }, 1500)
+    const timeoutId = setTimeout(() => {
+      router.push('/dashboard')
+    }, 1500)
 
-  //   return () => clearTimeout(timeoutId)
-  // }, [searchParams, router])
+    return () => clearTimeout(timeoutId)
+  }, [searchParams, router])
 
-  // return <SplashScreen />
+  return <SplashScreen />
 
-  // const acct = await createVirtualAccount({
-  //   customerCode: 'CUS_8hce0g9mj3foj0g',
-  //   preferredBank: 'titan-paystack',
-  //   first_name: 'Mathias',
-  //   last_name: 'Bala',
-  //   phone: '08123456789',
-  // })
-
-  // console.log(acct)
-
-  return <HomePage />
+  // return <HomePage />
 }
