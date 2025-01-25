1e0e1502a85c2034aa331423550e649167980e3b
Balamathias <balamathias05@gmail.com>
2025-01-25 21:14:41 +0100

    New branch instantiated

--- src/app/(root)/dashboard/share/page.tsx
+++ src/app/(root)/dashboard/share/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ShareAndEarnPage = () => {
   return (
     <div className='flex flex-col gap-y-4 md:py-12 mt-16'>
-      <h2>Share and Earn</h2>
+      <h2 className='text-2xl font-semibold'>Share and Earn</h2>
     </div>
   )
 }
