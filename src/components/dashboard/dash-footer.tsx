45958c8969786c44d6837dc40809bd0201b3383e
Balamathias <balamathias05@gmail.com>
2025-02-08 00:13:28 +0100

    New modella and fix timeout problema

--- src/components/dashboard/dash-footer.tsx
+++ src/components/dashboard/dash-footer.tsx
@@ -24,61 +24,28 @@ const DashFooter = () => {
         <Link href={`https://instagram.com/isubscribe_ng`} target='_blank' className='h-12 w-12 rounded-full bg-secondary flex items-center justify-center'>
           <Image src={`/glass/icons/Instagram.png`} alt='Instagram' width={24} height={24} />
         </Link>
-        <Link href={`https://wa.me/+2347049597498?text=Hello, isubcribe.`} target='_blank' className='h-12 w-12 rounded-full bg-secondary flex items-center justify-center'>
+        <Link href={`https://chat.whatsapp.com/FtUv7tE95Bt4vPbZ3DbNLS`} target='_blank' className='h-12 w-12 rounded-full bg-secondary flex items-center justify-center'>
           <Image src={`/glass/icons/Whatsapp.png`} alt='WhatsApp' width={24} height={24} />
         </Link>
+      </div>
+
+      <div className='flex items-center justify-center gap-x-3 mt-2.5'>
+        <div className='flex items-center gap-x-1'>
+          <Link href='/privacy-policy' className='text-muted-foreground hover:text-violet-500 transition-all'>
+            Privacy
+          </Link>
+          <span className='text-muted-foreground'>|</span>
+          <Link href='/about' className='text-muted-foreground hover:text-violet-500 transition-all'>
+            About
+          </Link>
+          <span className='text-muted-foreground'>|</span>
+          <Link href='/terms-and-conditions' className='text-muted-foreground hover:text-violet-500 transition-all'>
+            Terms & Conditions
+          </Link>
+        </div>
       </div>
     </motion.footer>
   );
 };
 
 export default DashFooter;
-
-
-  {/* <div className="flex flex-col items-center justify-center text-center">
-
-    <motion.div
-      className="flex space-x-6 mt-6 flex-wrap justify-center"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 0.4 }}
-    >
-      <Link
-        href="/privacy-policy"
-        className="flex items-center text-muted-foreground hover:text-violet-500 transition-all"
-      >
-        <Lock size={16} className="mr-2" /> Privacy
-      </Link>
-      <Link
-        href="/about"
-        className="flex items-center text-muted-foreground hover:text-violet-500 transition-all"
-      >
-        <Info size={16} className="mr-2" /> About
-      </Link>
-      <Link
-        href="/terms-and-conditions"
-        className="flex items-center text-muted-foreground hover:text-violet-500 transition-all"
-      >
-        <FileText size={16} className="mr-2" /> Terms & Conditions
-      </Link>
-    </motion.div>
-
-    <motion.p
-      className="mt-6 mb-3 text-sm text-muted-foreground"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 0.6 }}
-      >
-      &copy; {currentYear} isubscribe. All rights reserved.
-    </motion.p>
-
-    <motion.p
-      className="mt-3 mb-3 text-xs text-muted-foreground flex items-center gap-x-1.5 flex-wrap flex-col justify-center gap-2"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 0.6 }}
-      >
-      <LucideMapPin />
-      <span>Area 3, Garki, Abuja. <Link href={`tel:+2349154029723`} className='underline'>+234 915 4029 723</Link>, <Link href={`tel:+2349154029724`} className='underline'>+234 915 4029 724</Link>.</span>
-    </motion.p>
-  </div> */}
